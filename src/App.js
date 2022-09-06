9a0a9ade6f4535022164d670a0b189ba27edc61f
koechronix <koechronix@yahoo.com>
2022-09-05 22:18:52 -0400

    Imported components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
+import React, { useEffect, useState} from 'react';
+import ItemList from './components/ItemList'
 import Header from './components/Header';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/todos')
+    .then(r => r.json())
+    .then(todos => setTodos(todos))
+  }, [])
+
+  function handleAddTodo(newTodo) {
+    setTodos([...todos, newTodo])
+  }
+
+  const handleDeleteItem = (id) => {
+    const finalTodos = todos.filter(todo => todo.id !== id)
+    setTodos(finalTodos)
+  }
+
+  function handleUpdateTodo(updatedTodoObj) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === updatedTodoObj.id) {
+        return updatedTodoObj;
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-    </div>
-  );
+   <div className="main">
+     <Header todos={todos} onTodoDelete={handleDeleteItem}/>
+     <ItemList onAddTodo={handleAddTodo} todos={todos} onTodoDelete={handleDeleteItem} onUpdateTodo={handleUpdateTodo}/>
+     <br/><br/>
+   </div>
+  )
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Header({todos}) {
   return (
     <div>
-        <h1 className="header"> To Do Tasks</h1>
+        <h1 className="header"> To Do List</h1>
         <div>
-            <h2 className="all-tasks-title">All Tasks</h2>
+            <h2 className="all-todos-title">All Todos</h2>
+            {/* {renderTodosListAll} */}
         </div>
         </div>
   )
